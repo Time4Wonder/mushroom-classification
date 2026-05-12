8aa1a1afce472b7a5bd8f5f9a088676679d87aa9
t4w <lorenz.burgard@gmail.com>
2026-05-12 17:59:32 +0200

    wibed all project data, to start fresh in R

--- reader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from ucimlrepo import fetch_ucirepo 
-
-# 1. Daten laden
-mushroom = fetch_ucirepo(id=73) 
-X = mushroom.data.features 
-y = mushroom.data.targets 
-df = pd.concat([y, X], axis=1)
-
--- src/train_model.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-
-X_train = pd.read_csv("data/processed/X_train.csv")
-y_train = pd.read_csv("data/processed/y_train.csv")
-
-print(X_train)
