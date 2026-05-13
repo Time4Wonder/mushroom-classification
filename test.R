4519b542f31545edff81deaf0abcae0a1404a68e
t4w <lorenz.burgard@gmail.com>
2026-05-13 11:10:26 +0200

    test auslesen der daten.

---
+++ test.R
@@ -0,0 +1,27 @@
+# Mushroom Datensatz einlesen
+# Spaltennamen definieren (UCI Mushroom Dataset)
+col_names <- c(
+  "class",
+  "cap_shape", "cap_surface", "cap_color",
+  "bruises", "odor",
+  "gill_attachment", "gill_spacing", "gill_size", "gill_color",
+  "stalk_shape", "stalk_root",
+  "stalk_surface_above_ring", "stalk_surface_below_ring",
+  "stalk_color_above_ring", "stalk_color_below_ring",
+  "veil_type", "veil_color",
+  "ring_number", "ring_type",
+  "spore_print_color",
+  "population", "habitat"
+)
+
+# Datensatz einlesen
+mushroom <- read.csv(
+  "/home/t4w/development/ml-project/mushroom/agaricus-lepiota.data",
+  header = FALSE,
+  col.names = col_names,
+  stringsAsFactors = TRUE
+)
+
+# Überblick über den Datensatz
+str(mushroom)
+summary(mushroom)
